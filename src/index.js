a57e3413cf063cf3bd2cf883cfcbf1281c392e8f
Tiago <tigolimab223@gmail.com>
2022-06-20 16:12:39 -0300

    get and post routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import chalk from "chalk";
 
 const app = express();
 app.use(cors());
+app.use(json());
 
-app.get("")
+let users = [];
+let tweets = [];
+
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  if (user.username.length === 0) {
+    return res.send("Todos os campos são obrigatórios").status(400);
+  }
+  if (user.avatar.length === 0) {
+    return res.send("Todos os campos são obrigatórios").status(400);
+  }
+  users.push(user);
+  res.send(user).status(201);
+});
+
+app.post("/tweets", (req, res) => {
+  const tweet = {
+    username: req.body.username,
+    tweet: req.body.tweet,
+    avatar: users.find((user) => user.username === req.body.username).avatar,
+  };
+
+  if (tweet.username.length === 0) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
+
+  if (tweet.tweet.length === 0) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
+  tweets.unshift(tweet);
+  res.status(201).send(tweet);
+});
+
+app.get("/tweets", (req, res) => {
+  res.send(tweets).status(200);
+});
+
+app.get("/tweets/:username", (req, res) => {
+  let username = req.params.username;
+  let usernameMessages = tweets.filter((user) => username === user.username);
+  res.send(usernameMessages).status(200);
+});
 
 app.listen(5000, () => {
-    console.log(chalk.blue("Servidor funcionando na porta 5000"))
-})
+  console.log(chalk.blue("Servidor funcionando na porta 5000"));
+});
